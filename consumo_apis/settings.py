442a87b4713e428aeb606e7772ee83990c4a534b
ceciga <cecigallardo@gmail.com>
2025-05-05 20:10:10 -0300

    cambio configuracion settings

--- consumo_apis/settings.py
+++ consumo_apis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: v.split(','))
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=lambda v: v.split(','))
 
 
 # Application definition
